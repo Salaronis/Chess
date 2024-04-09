de0bdb3a35fb02a551daeccc71c4ce1c6bba6297
Michael Xie <mxie0114@glamil.com>
2024-04-09 03:03:09 -0400

    Adding White and black pieces

--- Chess.py
+++ Chess.py
@@ -5,9 +5,21 @@ WIDTH = 1000
 HEIGHT = 900
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 font = pygame.font.Font('freesansbold.ttf', 20)
+pygame.display.set_caption('Chess')
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
+
+white_pieces = ['rook' , 'knight' , 'bishop' , 'queen', 'king', 'bishop' , 'knight', 'rook', 
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+white_locations = [(0,0) , (1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0) , (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
+black_pieces = ['rook' , 'knight' , 'bishop' , 'queen', 'king', 'bishop' , 'knight', 'rook', 
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+black_locations = [(0,7) , (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7) , (0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)]
+
+captured_white = []
+captured_black = []
+
 
 run  = True
 while run:
